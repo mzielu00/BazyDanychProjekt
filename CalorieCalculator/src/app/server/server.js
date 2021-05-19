8dfcf72a50fd70828d0998483776fb63a2e2622f
mrcxmrj <marcin.zurawel@gmail.com>
2021-05-19 21:06:41 +0200

    added products.js

--- CalorieCalculator/src/app/server/server.js
+++ CalorieCalculator/src/app/server/server.js
@@ -13,22 +13,11 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Connected to database");
-});
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  category: String,
-  calories: Number,
-  proteins: Number,
-  carbohydrates: Number,
-  fats: Number,
-});
-
-const product = mongoose.model("product", productSchema);
-
-// The application is to listen on port number 3000
-app.listen(3000, function () {
-  console.log("The application is available on port 3000");
+  // The application is to listen on port number 3000
+  app.listen(3000, function () {
+    console.log("The application is available on port 3000");
+  });
 });
 
 app.get("/", (req, res) => {
