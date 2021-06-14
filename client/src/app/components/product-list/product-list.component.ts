c0e1e53223820bc08753b9908481eb6e27dd05b3
mrcxmrj <marcin.zurawel@gmail.com>
2021-06-14 22:37:59 +0200

    added removing from eaten list

--- client/src/app/components/product-list/product-list.component.ts
+++ client/src/app/components/product-list/product-list.component.ts
@@ -10,9 +10,7 @@ import { Product } from 'src/app/Product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-
 export class ProductListComponent implements OnInit {
-
   products: any;
   productList: Array<Product>;
   dataSource: any;
@@ -27,21 +25,20 @@ export class ProductListComponent implements OnInit {
   nameTerm: string;
   categoryTerm: string;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.showProducts();
     this.productList = new Array();
   }
 
-  showProducts(): void{
+  showProducts(): void {
     this.productService.getProducts().subscribe(
-      data =>
-      {
+      (data) => {
         this.products = data;
         console.log(data);
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
@@ -53,55 +50,53 @@ export class ProductListComponent implements OnInit {
     this.currentIdx = -1;
   }
 
-  setProduct(product: any, idx: number): void{
+  setProduct(product: any, idx: number): void {
     this.currProduct = product;
     this.currentIdx = idx;
     this.updateCounters();
   }
 
-  removeAll(): void{
+  removeAll(): void {
     this.productService.deleteProducts().subscribe(
-      response => {
+      (response) => {
         console.log(response);
         this.showProducts();
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
   }
   deleteProduct(name: any): void {
     this.productService.deleteProduct(name).subscribe(
-      response => {
+      (response) => {
         console.log(response);
         this.showProducts();
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
   }
   findProduct(): void {
     this.productService.findProductByName(this.name).subscribe(
-      data => {
+      (data) => {
         this.products = data;
         console.log(data);
       },
-      err => {
-        console.log(err)
+      (err) => {
+        console.log(err);
       }
     );
   }
 
   updateCounters(): void {
-
     this.caloriesCounter = 0;
     this.proteinsCounter = 0;
     this.carbohydratesCounter = 0;
     this.fatsCounter = 0;
 
-    for(var p of this.productList) {
-      
+    for (var p of this.productList) {
       this.caloriesCounter += p.calories;
       this.proteinsCounter += p.proteins;
       this.carbohydratesCounter += p.carbohydrates;
@@ -111,9 +106,17 @@ export class ProductListComponent implements OnInit {
 
   addProductToCalc(currProduct): void {
     this.productList.push(currProduct);
-    
-    console.log("im in addProductToCalc");
-    
+
+    console.log('im in addProductToCalc');
+
+    this.updateCounters();
+  }
+
+  removeFromEaten(product): void {
+    let index = this.productList.indexOf(product);
+    if (index > -1) {
+      this.productList.splice(index, 1);
+    }
     this.updateCounters();
   }
 }
